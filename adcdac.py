8d84cefd26240750285b04f741f3cfac48899b95
manitou48 <manitou@home>
2015-02-17 12:46:21 -0500

    update input interface

--- adcdac.py
+++ adcdac.py
@@ -10,23 +10,23 @@ class Bit:
     def __init__(self, auth_token, device_id):
         self.token = auth_token
         self.id = device_id
-        self.headers = {"Authorization": "Bearer " + self.token, "Accept": "application/vnd.littlebits.v2+json"}
+        self.headers = {"Authorization": "Bearer " + self.token}
     
     def output(self, pct, dur):
         r = requests.post("https://api-http.littlebitscloud.cc/devices/" + self.id + "/output", data={"percent": pct, "duration_ms": dur}, headers=self.headers)
         return r.text
 
     def input(self):
-        r = requests.get("https://api-http.littlebitscloud.cc/devices/" + self.id + "/input", headers=self.headers, stream=True)
+        r = requests.get("https://api-http.littlebitscloud.cc/v3/devices/" + self.id + "/input", headers=self.headers, stream=True)
         for line in r.iter_lines():
 			#if line: print line
 			yield line   # just fetch one line
         return
 
-auth = "whatever"
-id = "whatever2"
+auth = "ec77a8cae80d0699ba4082b23ec96ad72b97b814f2e5f3ed08fbaf69b4fac8dc"
+id = "243c200bffd5"
 bit = Bit(auth,id)
 while(1):
 	r=bit.input()
 	m = re.match(".*percent.:(\d+)",r.next())
-	if m: bit.output(m.group(1),2000)
+	if m: bit.output(m.group(1),3000)
